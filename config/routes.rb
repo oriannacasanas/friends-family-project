bde17df759161aa64d23f052a2bc0f63b2f50717
Orianna Casanas <orianna0826@gmail.com>
2017-02-12 14:20:11 -0500

    adding all files

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :posts
+  # resources :links
+
+  root to: "users#new"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  post '/login' => 'sessions#create'
+end
